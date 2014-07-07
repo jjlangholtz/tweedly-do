598f606d45d096d749109649c96c5cc4129c2e9c
Joshua Langholtz <jjlangholtz@gmail.com>
2014-07-07 00:03:17 -0400

    Added reveal modal for user login

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,15 +1,23 @@
 class LoginsController < ApplicationController
   def new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
     user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:password])
-      sign_in user
-      redirect_to user
-    else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
+    respond_to do |format|
+      if user && user.authenticate(params[:password])
+        sign_in user
+        format.html { redirect_to user }
+        format.js
+      else
+        flash.now[:error] = 'Invalid email/password combination'
+        format.html { render 'new' }
+        format.js
+      end
     end
   end
 
